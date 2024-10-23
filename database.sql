f89b2e62516db0656be9fe1d4c4e3734ee74f942
Aida <aidarile@gmail.com>
2024-10-23 11:31:34 +0200

    actualizo el sql con toda la info

--- database.sql
+++ database.sql
@@ -1,31 +1,157 @@
-CREATE DATABASE deportes_db;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-10-2024 a las 14:22:44
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
-USE deportes_db;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE socio (
-    id INT NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(128) NOT NULL,
-    telefono VARCHAR(9),
-    edad INT NOT NULL DEFAULT 0,
-    penalizado BOOLEAN NOT NULL DEFAULT FALSE,
-    PRIMARY KEY (id)
-);
 
-CREATE TABLE pista (
-    id INT NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(128) NOT NULL,
-    tipo VARCHAR(6) NOT NULL,
-    max_jugadores INT NOT NULL DEFAULT 2,
-    disponible BOOLEAN NOT NULL DEFAULT FALSE,
-    PRIMARY KEY (id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE reserva (
-    id INT NOT NULL AUTO_INCREMENT,
-    socio INT NOT NULL,
-    pista INT NOT NULL,
-    fecha VARCHAR(8) NOT NULL,
-    hora INT NOT NULL DEFAULT 0,
-    iluminar BOOLEAN NOT NULL DEFAULT FALSE,
-    PRIMARY KEY (id)
-);
+--
+-- Base de datos: `deportes_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pista`
+--
+
+CREATE TABLE `pista` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(128) NOT NULL,
+  `tipo` varchar(6) NOT NULL,
+  `max_jugadores` int(11) NOT NULL DEFAULT 2,
+  `disponible` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pista`
+--
+
+INSERT INTO `pista` (`id`, `nombre`, `tipo`, `max_jugadores`, `disponible`) VALUES
+(1, 'AZUL', 'PADEL', 4, 1),
+(2, 'VERDE', 'TENIS', 4, 1),
+(3, 'ROJO', 'FUTBOL', 12, 1),
+(4, 'MORADA', 'BASKET', 12, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reserva`
+--
+
+CREATE TABLE `reserva` (
+  `id` int(11) NOT NULL,
+  `socio` int(11) NOT NULL,
+  `pista` int(11) NOT NULL,
+  `fecha` varchar(8) NOT NULL,
+  `hora` int(11) NOT NULL DEFAULT 0,
+  `iluminar` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `reserva`
+--
+
+INSERT INTO `reserva` (`id`, `socio`, `pista`, `fecha`, `hora`, `iluminar`) VALUES
+(1, 4, 2, '29112024', 9, 0),
+(2, 1, 4, '28112024', 20, 1),
+(3, 3, 3, '30112024', 21, 1),
+(4, 2, 1, '20122024', 15, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `socio`
+--
+
+CREATE TABLE `socio` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(128) NOT NULL,
+  `telefono` varchar(9) DEFAULT NULL,
+  `edad` int(11) NOT NULL DEFAULT 0,
+  `penalizado` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `socio`
+--
+
+INSERT INTO `socio` (`id`, `nombre`, `telefono`, `edad`, `penalizado`) VALUES
+(1, 'Aida Garcia', '666666666', 20, 0),
+(2, 'Andrea Gomes', '777777777', 30, 0),
+(3, 'Ruben Garcia', '888888888', 30, 0),
+(4, 'Poe Gatete', '999999999', 13, 0);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `pista`
+--
+ALTER TABLE `pista`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FORANEA1` (`pista`),
+  ADD KEY `FORANEA2` (`socio`);
+
+--
+-- Indices de la tabla `socio`
+--
+ALTER TABLE `socio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `pista`
+--
+ALTER TABLE `pista`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `socio`
+--
+ALTER TABLE `socio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD CONSTRAINT `FORANEA1` FOREIGN KEY (`pista`) REFERENCES `pista` (`id`),
+  ADD CONSTRAINT `FORANEA2` FOREIGN KEY (`socio`) REFERENCES `socio` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
